e3e611fc3e1ed5d88c32801a5f5d4f148d576e0a
Samer Makni <dmc4samer@gmail.com>
2021-05-08 22:40:02 +0100

    Added Unit and feels like weather

--- main.js
+++ main.js
@@ -7,23 +7,29 @@ button.addEventListener("click", function (e) {
 });
 function getWeather(city) {
   var xhttp = new XMLHttpRequest();
+  var metric = true ;
+  var unit = metric ? " &#8451; " : " &#8457; " ;
+  var metricQuery = metric ? "&units=metric" : "" ; 
   const api_key = 'a96bee9b8b5588e77dafc135903ab8f4';
   const base_url= 'api.openweathermap.org/data/2.5/weather';
   xhttp.onreadystatechange = function () {
     try {
       if (this.readyState == 4) {
-        var result_contaier = document.getElementById("weather");
+        var result_container = document.getElementById("weather");
+        var feels_container = document.querySelector("#feels");
         var raw_data = JSON.parse(this.response);
-        result_contaier.innerHTML = raw_data.main.temp;
+        console.log(raw_data);
+        result_container.innerHTML = raw_data.main.temp + unit;
+        feels_container.innerHTML = raw_data.main.feels_like + unit;
       }
     }
     catch (error) {
-      result_contaier.innerHTML = "Non Existant";
+      result_container.innerHTML = "Non Existant";
     }
   };
   xhttp.open(
     "GET",
-    `//${base_url}?q=${city}&units=metric&appid=${api_key}`,
+    `//${base_url}?q=${city}${metricQuery}&appid=${api_key}`,
     true
   );
   xhttp.send();
