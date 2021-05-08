b0f31812ed4ae5000041696fba1d2d2cc88cdb79
SamerMakni <73069842+SamerMakni@users.noreply.github.com>
2021-05-08 22:12:44 +0100

    Updated API

--- main.js
+++ main.js
@@ -1,37 +1,36 @@
 var button = document.getElementById("result");
-button.addEventListener("click", function () {
-  console.log("clicked");
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
   var city = document.getElementById("city").value;
-  console.log(city);
   getWeather(city);
 });
 function getWeather(city) {
   var xhttp = new XMLHttpRequest();
+  const api_key = 'a96bee9b8b5588e77dafc135903ab8f4';
+  const base_url= 'api.openweathermap.org/data/2.5/weather';
   xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var weather = document.getElementById("weather");
-      var temp = JSON.parse(this.response);
-      console.log(temp);
-      try {
-          weather.innerHTML = temp.current.temperature;
-          
-      } catch (error) {
-          
+    try {
+      if (this.readyState == 4) {
+        var result_contaier = document.getElementById("weather");
+        var raw_data = JSON.parse(this.response);
+        result_contaier.innerHTML = raw_data.main.temp;
       }
-     try {if (temp.error.type == "request_failed") {
-        weather.innerHTML = "Non Existant";
-      }
-         
-     } catch (error) {
-         
-     }
-      
+    }
+    catch (error) {
+      result_contaier.innerHTML = "Non Existant";
     }
   };
   xhttp.open(
     "GET",
-    `http://api.weatherstack.com/current?access_key=03a74986671c5f411100cfd9cc98b6d9&query=${city}`,
+    `//${base_url}?q=${city}&units=metric&appid=${api_key}`,
     true
   );
   xhttp.send();
 }
+
+// api key: a96bee9b8b5588e77dafc135903ab8f4
+
+// https://api.openweathermap.org/data/2.5/weather?q=Tunis&units=metric&appid=a96bee9b8b5588e77dafc135903ab8f4
+
+// 
